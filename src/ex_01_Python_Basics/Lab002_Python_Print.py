7f515a8fc169d2d50d2b41b007c7501605b61689
BhaskarDivya <divyabhaskar22@gmail.com>
2026-03-07 21:50:42 +0530

    Added files - 7th March 26

--- src/ex_01_Python_Basics/Lab002_Python_Print.py
+++ src/ex_01_Python_Basics/Lab002_Python_Print.py
@@ -8,3 +8,16 @@ print("Jack"
 
 
       )
+
+# passing multiple number of arguments
+print(54, "Samuel", "James", True)
+#passing multiple number of arguments with seperator
+# for seperator default is space
+print("rose", "lotus", 100, "marigold", "lily", "jasmine", True, sep="")
+print("rose", "lotus", 100, "marigold", "lily", "jasmine", True, sep="*")
+print("rose", "lotus", 100, "marigold", "lily", "jasmine", True, sep="-")
+print("rose", "lotus", 100, "marigold", "lily", "jasmine", True, sep="-->")
+#passing multiple number of arguments with seperator and end
+# for end default is new line
+print("rose", "lotus", 100, "marigold", "lily", "jasmine", True, sep="-->",end='\n')
+print("James", "Peter", 56, "Mary", 25, True, sep="*", end='_')
